038275a359f51fc966894ca086f13c123444adbd
Geoffrey McGill <geoff@object.net>
2016-02-16 12:07:32 -0700

    Update to 3.3.0 release and year stamp

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,18 +5,14 @@ using System.Web.UI;
 [assembly: AssemblyTitle("Ext.NET Examples")]
 [assembly: AssemblyProduct("Ext.NET Examples")]
 [assembly: AssemblyDescription("Ext.NET Examples")]
-
-[assembly: AssemblyVersion("3.2.1.*")]
+[assembly: AssemblyVersion("3.3.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
-[assembly: AssemblyCopyright("Copyright © 2008-2015 Object.NET, Inc.")]
-
+[assembly: AssemblyCopyright("Copyright © 2008-2016 Object.NET, Inc.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
 [assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
-
 [assembly: WebResource("Ext.Net.Examples.Examples.DataView.Advanced.Multi_sort.multisortpanel.css", "text/css", PerformSubstitution = true)]
 [assembly: WebResource("Ext.Net.Examples.Examples.DataView.Advanced.Multi_sort.MultiSortPanel.js", "text/javascript")]
 [assembly: WebResource("Ext.Net.Examples.Examples.DataView.Advanced.Multi_sort.sortbutton.css", "text/css", PerformSubstitution = true)]
