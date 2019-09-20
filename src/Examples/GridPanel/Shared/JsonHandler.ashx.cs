55ed1f711b0c07ddf8812fa3c0ba8a5c06ddccbf
Fabrício Zimmerer Murta <fabricio@object.net>
2019-09-20 10:40:32 -0300

    Fixes invalid json being generated by JsonHandler web handler.
    The return payload was using single quotes in a json property name,
    which is not valid json, as far as google chrome's JSON.parse()
    implementation is concerned.

--- src/Examples/GridPanel/Shared/JsonHandler.ashx.cs
+++ src/Examples/GridPanel/Shared/JsonHandler.ashx.cs
@@ -15,7 +15,7 @@ namespace Ext.Net.Examples
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/json";
-            context.Response.Write(string.Format("{{'plants':{0}}}", JSON.Serialize(Plant.TestData)));
+            context.Response.Write(string.Format("{{\"plants\":{0}}}", JSON.Serialize(Plant.TestData)));
         }
 
         public bool IsReusable
